b311cf90cdefa88717f06450199868af9cad294e
Renato Lopes <renatolopes37@gmail.com>
2023-05-19 23:14:36 -0300

    feat: Created a Memory form with upload of image and video

---
+++ src/components/MessageLogin.tsx
@@ -0,0 +1,9 @@
+export function MessageLogin() {
+  return (
+    <div className="flex flex-1 items-center justify-center p-16">
+      <p className="w-[360px] text-center leading-relaxed">
+        Entre ou crie uma conta para salvar suas memórias...
+      </p>
+    </div>
+  )
+}
